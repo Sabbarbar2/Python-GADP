7a3183f052b75a1f598f35d2505e8acb2550a98b
Constantin Sabin <sabinconstantin60@yahoo.com>
2023-05-14 10:59:57 +0300

    v3

--- proiect_prezentare/todo_project/todo_app/views.py
+++ proiect_prezentare/todo_project/todo_app/views.py
@@ -20,7 +20,14 @@ def register(request):
 
 @login_required
 def task_list(request):
-    tasks = Task.objects.filter(user=request.user)
+    # tasks = Task.objects.filter(user=request.user)
+
+    current_user = request.user
+
+    if current_user.is_superuser:
+        tasks = Task.objects.all()
+    else:
+        tasks = Task.objects.filter(user=current_user)
 
     sort_by = request.GET.get('sort_by')
     if sort_by:
